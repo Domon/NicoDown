60fba34e61e1a4f6f498245c0a0dacde32822d2a
Chun-wei Kuo <Dendoh@gmail.com>
2009-09-18 15:02:04 +0800

    Add some codes to get video download url

--- nicodown.user.js
+++ nicodown.user.js
@@ -7,11 +7,17 @@
 console.log("NicoDown loaded.");
 var nico = "http://www.nicovideo.jp";
 var video_id = "nm5074235";
+var download_url = "";
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange=function(){
 	console.log("readystate: " + xhr.readyState);
 	if (xhr.readyState == 4) {
 		console.log("responseText: " + xhr.responseText);
+		var response = xhr.responseText;
+		var start = response.indexOf("&url=") + 5;
+		var stop = response.indexOf("&", start);
+		download_url = response.substring( start, stop );
+		console.log("download_url = " + download_url;
 	}
 }
 xhr.open("GET", nico + "/api/getflv/" + video_id, true);
