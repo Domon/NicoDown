dda3e78e697f3e2a3d6f8902bacf820ce7892faa
Chun-wei Kuo <Dendoh@gmail.com>
2009-09-18 13:34:56 +0800

    Modified match url

--- nicodown.user.js
+++ nicodown.user.js
@@ -2,6 +2,6 @@
 // @name	NicoDown
 // @description	A simple user script which provides niconico video URLs
 // @author	Domon
-// @match	http://www.nicovideo.jp/watch*
+// @match	http://www.nicovideo.jp/watch/*
 // ==/UserScript==
 console.log("NicoDown loaded.");
